3f1b7163c4e56ac2d8bc439531e9ee0c06fa3b72
androxiz <8466486@stud.nau.edu.ua>
2025-01-18 20:48:36 +0200

    added a following functionslity(with help) and fixed followers counter

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -58,3 +58,29 @@ $(document).on("click", '.js-toggle-modal', function(e){
         }
     });
 })
+.on('click', '.js-follow', function(e) {
+    e.preventDefault();
+    console.log('Clicked')
+    const action = $(this).attr('data-action')
+    $.ajax({
+        type: 'POST',
+        url: $(this).data('url'),
+        data: {
+            action: action,
+            username: $(this).data('username'),
+        },
+        success: (data) => {
+            $('.js-follow-text').text(data.wording)
+            if(action=='follow'){
+                //Change wording to unfollow
+                $(this).attr('data-action', 'unfollow')
+            }else{
+                //Change wording to follow
+                $(this).attr('data-action', 'follow')
+            }
+        },
+        error: (error) => {
+            console.warn(error)
+        }
+    });
+})
