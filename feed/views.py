ab1d30270b214bcb94561bea6060a3cb396d494f
androxiz <8466486@stud.nau.edu.ua>
2025-01-18 14:11:47 +0200

    added a login-required rule to make a new posts

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Post
 
@@ -19,7 +21,7 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
 
 
-class CreateNewPost(CreateView):
+class CreateNewPost(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'feed/create.html'
     fields = ['text']
